7488a7ed858a664dc590c213270e5529ea3c0c35
ahmadmolaie <molaie@gmail.com>
2015-06-23 21:01:07 +0430

    just test

---
+++ Mission Impossible/Mission Impossible/UIControlsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Mission_Impossible {
+	public class UIControlsViewModel {
+
+		private ObservableCollection<FrameworkElement> views =	new ObservableCollection<FrameworkElement>();
+
+
+	}
+}
